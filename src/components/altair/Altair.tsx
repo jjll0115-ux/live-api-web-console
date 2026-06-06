02b9b22c245f2b4134f8dff7427915141c8c1de3
jjll0115-ux <jjll0115@gmail.com>
2026-06-06 11:06:31 +0800

    Update Altair.tsx
    gemini-live-2.5-flash-native-audio

--- src/components/altair/Altair.tsx
+++ src/components/altair/Altair.tsx
@@ -44,7 +44,7 @@ function AltairComponent() {
   const { client, setConfig, setModel } = useLiveAPIContext();
 
   useEffect(() => {
-    setModel("models/gemini-3-flash-preview");
+    setModel("models/gemini-live-2.5-flash-native-audio");
     setConfig({
       responseModalities: [Modality.AUDIO],
       speechConfig: {
